21817708a275ceaaf4e2df24864ad6c0ec19d3aa
diegomaranges <32415456+diegomaranges@users.noreply.github.com>
2021-02-01 18:50:10 -0300

    new directory file display

---
+++ 2_Febrero/Encuentro_01_02_While_Switch/script/script.js
@@ -0,0 +1,16 @@
+/*var num1S = prompt("ingrese el primer numero", '123');//123 123 => 4
+var num1N = parseInt(num1S); //123 => 3
+
+var num2S = prompt("ingrese el segundo numero", "123");
+var num2N = parseInt(num2S);
+
+var mensaje;
+
+if (!isNaN(num1N) && !isNaN(num2N) && num1N.toString().length === num1S.length && num2N.toString().length === num2S.length) { //123a num1
+    mensaje = "la sumatoria es: " + (num1N + num2N);
+} else {
+    mensaje = "uno de los valores es erroneo";
+}
+
+alert(mensaje);
+*/
