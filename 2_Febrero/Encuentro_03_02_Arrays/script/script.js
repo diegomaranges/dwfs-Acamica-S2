490752337d0255e624484efd1ee5bb732dab7590
diegomaranges <32415456+diegomaranges@users.noreply.github.com>
2021-02-03 23:38:22 -0300

    functions created in programan part

--- 2_Febrero/Encuentro_03_02_Arrays/script/script.js
+++ 2_Febrero/Encuentro_03_02_Arrays/script/script.js
@@ -55,3 +55,109 @@ function aFunction() {
     console.log(varConst + "  hola");
     alert(typeof(varFunction[5]));
 }
+
+function programamos() {
+    const testArray = [1, 2, 3, "asdb", "aiz", 4, 5, "hhhh", "xxxx", "zzz", 6, 30, 129, "njcxnjoasc"];
+    let sumOfElements = 0; //la suma de todos los elementos numericos
+    let otherElements = []; //nuevo arreglo con el resto de los elementos no numericos
+    for (const element of testArray) {
+        if (typeof(element) === "number") {
+            sumOfElements = sumOfElements + element;
+        } else {
+            otherElements.push(element);
+        }
+    }
+    console.log(sumOfElements);
+    console.log(otherElements);
+}
+
+function stop() {
+    /* Ingresar mediante prompt números indefinidamente hasta que el usuario ingrese “Stop”.
+    Almacenar todos los datos en un array y mostrarlos al final. */
+    let numeroIngresado;
+    let promtUsuario;
+    let arrayFinal = [];
+    do {
+        promtUsuario = prompt("inserte un numero"); // "hola"
+        numeroIngresado = parseInt(promtUsuario); // nan
+        if (!isNaN(numeroIngresado)) { // is nan? -> si -> true -> not true por la negacion -> false
+            arrayFinal.push(numeroIngresado); // no se ejecuta
+        }
+    } while (promtUsuario !== "Stop"); // "hola" !==  strop -> true
+    console.log(arrayFinal);
+}
+
+function dividir_array() {
+    /* Ingresar mediante prompt números indefinidamente hasta que el usuario ingrese 0 (cero).
+    Crear un array para los números pares, otro para los impares y un tercero para almacenar datos no numéricos. Mostrar los resultados al finalizar el ciclo.
+    */
+    let numeroIngresado;
+    let promtUsuario;
+    let parArray = [];
+    let imparArray = [];
+    let otroArray = [];
+    do {
+        // promtUsuario.length === numeroIngresado/length -> si es true lo que ingreso el usuario fue un numero
+        promtUsuario = prompt("inserte un numero"); // -> "321abc" -> "0" -> "0asd"
+        numeroIngresado = parseInt(promtUsuario); // 321
+
+        if (promtUsuario === "0") {
+            break;
+        }
+
+        if (numeroIngresado % 2 === 0) { //es par
+            parArray.push(numeroIngresado);
+        } else if (numeroIngresado % 2 === 1) { //es impar
+            imparArray.push(numeroIngresado);
+        } else { // otherwise
+            otroArray.push(promtUsuario);
+        }
+
+    } while (true);
+    console.log(parArray);
+    console.log(imparArray);
+    console.log(otroArray);
+}
+
+/* Ingresar mediante prompt cualquier carácter hasta el ingreso de un 0 (cero).
+Almacenar cada ingreso en una posición del array.
+Al finalizar dividir la cantidad de elementos en 3 arrays iguales
+ */
+function dividir_array_3() {
+    let numeroIngresado;
+    let promtUsuario;
+    let myArray = [];
+    do {
+        // promtUsuario.length === numeroIngresado/length -> si es true lo que ingreso el usuario fue un numero
+        promtUsuario = prompt("inserte un numero"); // -> "321abc" -> "0" -> "0asd"
+        numeroIngresado = parseInt(promtUsuario); // 321
+
+        if (promtUsuario === "0") {
+            break;
+        }
+
+        if (!isNaN(numeroIngresado)) { //es par
+            myArray.push(numeroIngresado);
+        } else { // otherwise
+            myArray.push(promtUsuario);
+        }
+
+    } while (true);
+    //dividir en 3 mi array e imprimirlo
+    let numberOfElements = myArray.length / 3;
+    if (myArray.length % 3 === 0) {
+        console.log(myArray.splice(0, numberOfElements));
+        console.log(myArray.splice(0, numberOfElements));
+        console.log(myArray.splice(0, numberOfElements));
+    } else if (myArray.length % 3 === 1) {
+        console.log(myArray.splice(0, numberOfElements + 1));
+        console.log(myArray.splice(0, numberOfElements));
+        console.log(myArray.splice(0, numberOfElements));
+    } else {
+        console.log(myArray.splice(0, numberOfElements + 1));
+        console.log(myArray.splice(0, numberOfElements + 1));
+        console.log(myArray.splice(0, numberOfElements));        
+    }
+
+
+}
