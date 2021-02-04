f59ae232b0670d3006bdb3eedf0c9a72158d9247
diegomaranges <32415456+diegomaranges@users.noreply.github.com>
2021-02-03 21:03:31 -0300

    start with arrays

---
+++ 2_Febrero/Encuentro_03_02_Arrays/script/script.js
@@ -0,0 +1,15 @@
+/*
+scope - var, let y const
+let mi_array = [];
+let mi_array2 = new Array();
+acceder a posición mi_array[5]
+length
+unshift
+push
+indexOf
+shift
+pop
+splice
+join
+sort - reverse
+*/
