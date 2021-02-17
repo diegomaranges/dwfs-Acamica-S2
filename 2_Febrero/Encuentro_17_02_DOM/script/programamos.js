b14d70f52a23d9bf3ae50a82c97098cdca28de15
diegomaranges <32415456+diegomaranges@users.noreply.github.com>
2021-02-17 20:19:48 -0300

    create folder and base for today

---
+++ 2_Febrero/Encuentro_17_02_DOM/script/programamos.js
@@ -0,0 +1,10 @@
+/*
+Pide al usuario estos 4 datos:
+
+Nombre y apellido
+Background color
+Font color
+Font size
+
+Dibuja un cuadrado con el nombre y apellido en el centro y aplica las propiedades que solicitaste modificando el DOM.
+*/
---
+++ 2_Febrero/Encuentro_17_02_DOM/script/script.js
@@ -0,0 +1,25 @@
+/*
+Propiedades de los nodos (obtener valor o asignar):
+element.attributes, element.className, element.classList, element.firstElementChild,
+element.id, element.innerHTML, element.lastElementChild, element.name, element.outerHTML, 
+element.tagName 
+
+Animar elementos: element.animate(keyframes, options)  
+
+Modificar nodos: element.remove(), element.replaceWith(nuevoNodo)
+
+Crear nuevos elementos: document.createElement(tagName)
+
+Insertar nodos: element.appendChild(), element.before(), element.after(), element.insertBefore(), 
+replaceChild
+element.setAttribute(name, value) 
+tomar el valor de un atributo (rel, id, class)
+tener un ejemplo específico del atributo value
+
+localStorage
+setItem, getItem y removeItem
+
+sessionStorage
+setItem, getItem y removeItem
+*/
+
